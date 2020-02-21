48d8f7570d0c7ec458fc18735b0f7d5f914b8e49
Dakota Willingham <kota@vaga.tk>
2020-02-21 21:17:38 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,10 +9,5 @@ end
 
 def print_first_directors_movie_titles
   directors = pretty_print_nds(nds)
-  count = 0
-  if directors[:name] = "Stephen Spielberg"
-    while count < directors[:name].length do
-      
-    end
-  end
+  
 end
